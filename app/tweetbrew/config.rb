53dfa85a022464c9d021f4f65471aed9248eff96
Xu Cheng <xucheng@me.com>
2015-05-14 12:24:54 +0800

    update twitter account

--- app/tweetbrew/config.rb
+++ app/tweetbrew/config.rb
@@ -1,5 +1,5 @@
 # twitter accounts used in official homebrew taps
 TWITTER_ACCOUNT_MAP = {
-  "homebrew" => "MacHomebrew",
+  "homebrew" => "HomebrewFormula",
   "homebrew-science" => "brew_sci",
 }
